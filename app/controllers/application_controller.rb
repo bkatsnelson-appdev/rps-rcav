49da9bfe4fd2b103cef8b996985e216b0a433a3d
beatricek222 <beatricek222@gmail.com>
2022-10-13 21:03:28 +0000

    Lots of progress on paper and setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,33 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    self.render({ :template => "game_templates/rules.html.erb" })
+  end
+
   def play_rock
     #write code
     self.render({ :template => "game_templates/user_rock.html.erb" })
     #self.render({ :html => "<h1>Hello, world!</h1>".html_safe })
     #self.redirect_to("https://www.wikipedia.org/")
     #self.render({ :plain => "Howdy, world!" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    
+    self.render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    self.render({ :template => "game_templates/user_scissors.html.erb" })
   end
 end
